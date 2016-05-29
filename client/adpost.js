e6122de6c5628b06f581d246f05966ff27c15908
Aniket Limaye <allimaye@uwaterloo.ca>
2016-05-29 16:14:11 -0400

    Image preview now refreshes after every image upload.

--- client/adpost.js
+++ client/adpost.js
@@ -225,9 +225,16 @@
         };
 
         $scope.removeBook = function (index) {
+
+            //at least one book is needed
+            if ($scope.textbooks.length == 1)
+            {
+                return;
+            }
             $scope.textbooks.splice(index, 1)
             $scope.$evalAsync(function () {
                 $timeout(bindButtonsToFileInputs, 100);
+                $timeout(generateImagePreview, 100);
             });
         };
 
@@ -268,6 +275,10 @@
                     fileInput.click();
                     $(this).text("UPLOAD PICTURE");
                     $scope.textbooks[index].picture = null;
+
+                    $scope.$evalAsync(function () {
+                        $timeout(generateImagePreview, 1000);
+                    });
                 });
 
                 var fileUploadButton = $(this);
@@ -276,6 +287,10 @@
                     if(fileInput[0].files[0])
                     {
                         fileUploadButton.text(fileInput[0].files[0].name);
+
+                        $scope.$evalAsync(function () {
+                            $timeout(generateImagePreview, 100);
+                        });
                     }
                 });
 
@@ -286,82 +301,19 @@
 
         $scope.onFormSubmit = function () {
 
-             if (!$scope.adForm.$valid)
-             {
-                 $scope.adForm.$submitted = true;
-                 return;
-             }
-
-            var images = [];
-            var canvas = document.getElementById('canvas');
-            var context = canvas.getContext('2d');
-
-
-            //convert File objects to Image objects
-            $.each($scope.textbooks, function (index, value) {
-                if(value.picture)
-                {
-                    var newImg = new Image();
-                    newImg.src = URL.createObjectURL(value.picture);
-                    images.push({
-                        img: newImg,
-                        width: null,
-                        height: null,
-                        aspectRatio: function() {
-                           return this.width/this.height;
-                        }
-                    });       
-                }
-            });
-
-            if(images.length > 0)
+            if (!$scope.adForm.$valid)
             {
-                //calculate presetWidth based on browser space and number of images to be filled
-                var card = $("#canvasContainer");
-                var cardPadding = parseInt(card.css('padding-right')) + parseInt(card.css('padding-left'));
-                var space = $("#canvasContainer")[0].clientWidth - cardPadding;
-                //width of uploaded images during concatenation
-                var presetWidth = space/images.length; 
-
-                $.each(images, function (index, image) {
-                    image.img.onload = function() {
-                        image.width = this.width;
-                        image.height = this.height;
-                    };
-                });
-                
-                $scope.$evalAsync(function(){
-                    $timeout(loadImages, 500);
-                });
-                
-            }
-            else
-            {
-                promptUser(false);
+                $scope.adForm.$submitted = true;
+                return;
             }
 
-            function loadImages()
-            {
-                var potentialHeight = getMaxCanvasHeight(images, presetWidth);
-                canvas.height = potentialHeight < ($(window).height() * 0.5) ? potentialHeight : $(window).height() * 0.5;
+            var result = generateImagePreview();
+            promptUser(result);
 
-                //canvas.height = getMaxCanvasHeight(images, presetWidth);
-                canvas.width = adjustCanvasWidthBasedOnHeight(images, canvas.height);
-                var widthTaken = 0;
-                $.each(images, function (index, image) {
-                    context.drawImage(image.img, widthTaken, 0, canvas.height * image.aspectRatio(), canvas.height);
-                    widthTaken += canvas.height * image.aspectRatio();
-                    //clean up temp URL
-                    URL.revokeObjectURL(image.img.src);
-                });
-
-                promptUser(true);
-            }
-
-            function promptUser(userUploadImages)
+            function promptUser(userUploadedImages)
             {
                 var dialogTitle = "", dialogText = "";
-                if (userUploadImages)
+                if (userUploadedImages)
                 {
                     dialogTitle = 'Do you like what you see?';
                     dialogText = 'Are you ok with posting the concatenated ' +
@@ -386,9 +338,8 @@
 
                 $mdDialog.show(confirm).then(function() {
                     var postMsg = composePostMessage();
-                    var includeImage = images.length > 0;
 
-                    if(includeImage)
+                    if (userUploadedImages)
                     {
                         $mdDialog.show(
                           $mdDialog.alert()
@@ -416,7 +367,7 @@
                                     var liveURL = response.data.link;
                                     $scope.postObject = {
                                         message: postMsg,
-                                        includeImg: includeImage,
+                                        includeImg: userUploadedImages,
                                         url: liveURL,
                                         caption: "View Full Image on Imgur"
                                     };
@@ -430,7 +381,7 @@
                     {
                         $scope.postObject = {
                             message: postMsg,
-                            includeImg: includeImage,
+                            includeImg: userUploadedImages,
                             url: null,
                             caption: "View Full Image on Imgur",
                         };
@@ -462,21 +413,99 @@
             });
         }
 
+        function generateImagePreview() {
+            var images = [];
+            getImagesFromFiles(images);
+
+            //if there are no images to collage/preview, then clear canvas and exit
+            if (images.length < 1) {
+                var canvas = document.getElementById('canvas');
+                var context = canvas.getContext('2d');
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                return false;
+            }
+
+            var presetWidth = getPresetWidth(images);
+
+            var counter = 0;
+
+            //initialize image attributes on load
+            $.each(images, function (index, image) {
+                image.img.onload = function () {
+                    image.width = this.width;
+                    image.height = this.height;
+                    counter++;
+                    
+                    //display images onto canvas if this is the last image to be loaded
+                    if (counter == images.length)
+                    {
+                        $scope.$evalAsync(function () {
+                            $timeout(loadImages(images, presetWidth), 100);
+                        });
+                    }
+
+                };
+            });
+
+
+            return true;
+        }
+
+        function loadImages(images, presetWidth) {
+            var canvas = document.getElementById('canvas');
+            var context = canvas.getContext('2d');
+            var potentialHeight = getMaxCanvasHeight(images, presetWidth);
+            canvas.height = potentialHeight < ($(window).height() * 0.5) ? potentialHeight : $(window).height() * 0.5;
+            canvas.width = adjustCanvasWidthBasedOnHeight(images, canvas.height);
+            var widthTaken = 0;
+            $.each(images, function (index, image) {
+                context.drawImage(image.img, widthTaken, 0, canvas.height * image.aspectRatio(), canvas.height);
+                widthTaken += canvas.height * image.aspectRatio();
+                //clean up temp URL
+                URL.revokeObjectURL(image.img.src);
+            });
+        }
+
+        function getImagesFromFiles(images) {
+            //convert File objects to Image objects
+            $.each($scope.textbooks, function (index, value) {
+                if (value.picture) {
+                    var newImg = new Image();
+                    newImg.src = URL.createObjectURL(value.picture);
+                    images.push({
+                        img: newImg,
+                        width: null,
+                        height: null,
+                        aspectRatio: function () {
+                            return this.width / this.height;
+                        }
+                    });
+                }
+            });
+        }
+
+        function getPresetWidth(images) {
+            //calculate presetWidth based on browser space and number of images to be filled
+            var card = $("#canvasContainer");
+            var cardPadding = parseInt(card.css('padding-right')) + parseInt(card.css('padding-left'));
+            var space = $("#canvasContainer")[0].clientWidth - cardPadding;
+            //width of uploaded images during concatenation
+            var presetWidth = space / images.length;
+            return presetWidth;
+        }
+
         function getMaxCanvasHeight(images, presetWidth) {
             var max = 0;
-
             $.each(images, function (index, image) {
                 var presetHeight = presetWidth/image.aspectRatio();
                 if(presetHeight > max) max = presetHeight;
             });
-
             return max;
         }
 
         function adjustCanvasWidthBasedOnHeight(images, canvasHeight)
         {
             var total = 0;
-
             $.each(images, function (index, image) {
                 total += canvasHeight * image.aspectRatio();
             });
@@ -622,105 +651,3 @@
 
 })();
 
-
-/* ========= Potentially useful code ======
-
-- image resizing (step down technique)
-
-// // set main canvas size proportional to image
-// canvas.height = canvas.width * (addImg.height / addImg.width);
-// // context.drawImage(addImg, 0, 0, canvas.width, canvas.height);
-
-// //Step 1 - resize to 50% of original. This is done by first halving the canvas width/height,
-// //and then adding the image to the modified canvas
-// var oc = document.createElement('canvas'),
-//     octx = oc.getContext('2d');
-
-// oc.width = addImg.width * 0.5;
-// oc.height = addImg.height * 0.5;
-// octx.drawImage(addImg, 0, 0, oc.width, oc.height);
-
-// //Step 2 - resize to 50% of Step 1. Draw the half size canvas onto itself,
-// //once again halving dimensions as its done.
-// octx.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5);
-
-// //Step 3, resize to final size. Draw temp canvas into final canvas using clipping.
-// //Start clipping at (0,0) and only use half of temp canvas dimensions since that is
-// //the only part of the canvas that was filled in Step 2. 
-// context.drawImage(oc, 0, 0, oc.width * 0.5, oc.height * 0.5,
-// 0, 0, canvas.width, canvas.height);
-
-
-
-// var addImg = new Image();
-
-// addImg.onload = function() {
-//     addImg.style.width = presetWidth+"px";
-//     addImg.style.height = "auto";
-//     context.drawImage(addImg, index*presetWidth, 0, presetWidth, canvas.height);
-// };
-
-// addImg.src = URL.createObjectURL(img);
-
-
-//FB.api("/debug_token?input_token="+authSvc.auth.accessToken,
-//     function (response) {
-//        if (response && !response.error)
-//        {
-//            console.log(response);
-//        }
-//});
-
-                
-
-//FB.login(function (response) {
-//    authSvc.auth = response.authResponse;
-//    authSvc.user = { "userID": response.authResponse.userID };
-//    console.log("AuthResponse: " + response.authResponse);
-//}, {
-//    scope: 'publish_actions,user_managed_groups',
-//    return_scopes: true
-//});
-
-
-//FB.api("/"+authSvc.user.userID + "/groups",
-                //    function (response) {
-                //        console.log(response);
-                //        if (response && !response.error) {
-                //            
-                //        }
-                //});
-
-                //FB.api("/"+groupId+"/feed",
-                //        "POST",
-                //        {
-                //            "message": "This is a test message",
-                //            "link": "http://www.revitalizenotmilitarize.org/wp-content/uploads/2013/10/FlowerPower_v1.png",
-                //            "caption": "View full image on Imgur"
-                //        },
-                //        function (response) {
-                //            console.log(response);
-                //            if (response && !response.error)
-                //            {
-                            
-                //                
-                //            }
-                // });
-
-
-
-                //$.ajax({
-                //    url: 'https://graph.facebook.com/v2.6/oauth/access_token',
-                //    type: 'get',
-                //    dataType: 'json',
-                //    data: {
-                //        "grant_type": "fb_exchange_token",
-                //        "client_id": "",
-
-                //    },
-                //    success: function (data) {
-                //        console.log(data);
-                //    }
-                //});
-
-*/
